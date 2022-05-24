7b2f1c2a7177868992285c473834b073eb44c792
stujazan <102478200+stujazan@users.noreply.github.com>
2022-05-25 01:14:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ trestbps = st.number_input('Resting Blood Pressure')
 #Cholesterol
 chol = st.number_input('Serum Cholesterol')
 #FastingBS
-fbs = st.selectbox('Fasting Blood Sugar',[0,1])
+fbs = st.selectbox('Fasting Blood Sugar',["mal","fimal"])
 #RestingECG
 restecg = st.selectbox('Resting Electrocardiogram Results',df['restecg'].unique())
 #MaxHR
