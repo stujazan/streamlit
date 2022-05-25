6d4ad72ac911a5f1b53f1884627fbb830ac34081
stujazan <102478200+stujazan@users.noreply.github.com>
2022-05-25 20:01:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ import numpy as np
 pipe = pickle.load(open('pipe.pkl','rb'))
 df = pickle.load(open('df.pkl','rb'))
 
-st.title("Heart Disease Predictor by Jazan studen")
+st.title("Heart Disease Predictor by Jazan student")
 # st.text("1 means you've a heart disease, 0 means you don't have a heart disease")
 #Age
 Age = st.number_input('Age of Person')
 #Sex
-Sex = st.selectbox('Sex')
+Sex = st.selectbox('Sex',df['sex'].unique())
 #ChestPainType
 cp = st.selectbox('Chest Pain Type',df['cp'].unique())
 #RestingBP
@@ -20,7 +20,7 @@ trestbps = st.number_input('Resting Blood Pressure')
 #Cholesterol
 chol = st.number_input('Serum Cholesterol')
 #FastingBS
-fbs = st.selectbox('Fasting Blood Sugar',[1,0])
+fbs = st.selectbox('Fasting Blood Sugar',[0,1])
 #RestingECG
 restecg = st.selectbox('Resting Electrocardiogram Results',df['restecg'].unique())
 #MaxHR
@@ -32,16 +32,18 @@ Oldpeak = weight = st.number_input('Old peak')
 #ST_Slope
 slope = st.selectbox('The slope of the peak exercise ST segment',df['slope'].unique())
 if st.button('Predict Heart Health'):
-    if (Age > 65 and trestbps > 170 and chol > 265 and thalach > 210):
-       st.header("You've a heart disease. Please, consult your doctor")
-    else :
-       query = np.array([Age,Sex,cp,trestbps,chol,fbs,restecg,thalach,exang,Oldpeak,slope])
-       query = query.reshape(1, 11)
-       result=str(pipe.predict(query)[0])
-       if result == 0:
-          st.header("You've a heart disease. Please, consult your doctor")
-       else :
-           st.header("You don't have a heart disease")
+    query = np.array([Age,Sex,cp,trestbps,chol,fbs,restecg,thalach,exang,Oldpeak,slope])
+    query = query.reshape(1, 11)
+    result=str(pipe.predict(query)[0])
+    if result == 1:
+        st.header("You've a heart disease. Please, consult your doctor")
+    else:
+        st.header("You don't have a heart disease")
+    # st.title("Your Heart Health is " + str(pipe.predict(query)[0]))
+    # query = query.reshape(1, 11)
+    # st.title("Your Heart Health Status is "+ str(int(pipe.predict(query)[0])))
+    # st.header("Your Heart Health(1 means you've a disease) Status is:" + str(int(result[0])))
+
             
 
 
